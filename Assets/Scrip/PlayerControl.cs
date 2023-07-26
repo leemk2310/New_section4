9052f0c2a9ebd31b5bfcba9e5cf02db933aad7d4
leemk2310 <lee.minhkhanh@gmail.com>
2023-07-26 17:54:54 +0900

    rotation

--- Assets/Scrip/PlayerControl.cs
+++ Assets/Scrip/PlayerControl.cs
@@ -1,32 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
+
 
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] InputAction Movement;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-      private void OnEnable()
-       {
-        Movement.Enable();
+    
+   [SerializeField] float Movespeed =5f;
+   [SerializeField] float  Xrank = 10f;
+   [SerializeField] float Yrank =10f;
+   [SerializeField] float Pitchfactor =-2f;
+   [SerializeField] float PitchControl =5f;
+   [SerializeField] float Yawfactor = -2f;
 
-     }
-      private void OnDisable() {
-        Movement.Disable();
-      }
-
+   [SerializeField] float RollControl =5f;
     // Update is called once per frame
-    void Update()
+ float HorizontalThrow;
+ float Verticalthrow;
+void Update()
+{
+    tranformlocation();
+    tranformRotation();
+}
+
+void tranformRotation()
+{
+    float Pitch = transform.localPosition.y *Pitchfactor  + Verticalthrow*PitchControl;
+    float Yaw = transform.localPosition.x * Yawfactor;
+    float Roll = HorizontalThrow * RollControl;
+    transform.localRotation = Quaternion.Euler(Pitch, Yaw, Roll);
+
+}
+    void tranformlocation()
     {
-        float HorizontalThrow = Movement.ReadValue<Vector2>().x;
-        Debug.Log(HorizontalThrow);
-        float Verticalthrow = Movement.ReadValue<Vector2>().y;
-        Debug.Log(Verticalthrow);
+         HorizontalThrow = Input.GetAxis("Horizontal");
+         Verticalthrow = Input.GetAxis("Vertical");
+
+        float Xput = HorizontalThrow * Time.deltaTime * Movespeed; // thuc hien 1 action move
+
+        float Xnewpost = transform.localPosition.x + Xput; // check vi tri hien tai va move toi vi tri  khi thuc hien action
+
+        float XnewpostRank = Mathf.Clamp(Xnewpost, -Xrank, Xrank);  //set rank khi move
+
+        float Yput = Verticalthrow * Time.deltaTime * Movespeed;
+        float Ynewpost = transform.localPosition.y + Yput;
+        float YnewpostRank = Mathf.Clamp(Ynewpost, -Yrank, Yrank);
+
+
+        transform.localPosition = new Vector3(XnewpostRank, YnewpostRank, transform.localPosition.z);
+
+
+
     }
+
 }
