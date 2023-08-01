68eacf2238efd06991d06b2aebe9095be323be5f
leemk2310 <139373389+leemk2310@users.noreply.github.com>
2023-08-01 18:10:06 +0900

    Update CollisionMove.cs
    Update Reload level

--- Assets/Scrip/CollisionMove.cs
+++ Assets/Scrip/CollisionMove.cs
@@ -1,14 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionMove : MonoBehaviour
 {
-     void OnTriggerEnter(Collider other) 
+   [SerializeField] float freezetime  = 5f;
+    void OnTriggerEnter(Collider other) 
     {
-      Debug.Log($"{this.name}**Trigger by **{other.gameObject.name}");  
+      Debug.Log($"{this.name} **Trigger by **{other.gameObject.name}");  
+      FreezeAction();
+      
+    }
+    void FreezeAction()
+    {
+        GetComponent<PlayerControl>().enabled = false;
+        Invoke("ReloadLevel", freezetime);
+    }
+
+      void ReloadLevel()
+
+    {
       int reloadLevel = SceneManager.GetActiveScene().buildIndex;
       SceneManager.LoadScene(reloadLevel);
-
     }
-}
+ }
