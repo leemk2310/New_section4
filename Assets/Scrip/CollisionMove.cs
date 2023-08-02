410edf04d140d9e55853499442d8fdb909c980cd
leemk2310 <139373389+leemk2310@users.noreply.github.com>
2023-08-02 17:46:49 +0900

    Update CollisionMove.cs

--- Assets/Scrip/CollisionMove.cs
+++ Assets/Scrip/CollisionMove.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class CollisionMove : MonoBehaviour
 {
    [SerializeField] float freezetime  = 2f;
+   [SerializeField] ParticleSystem lossee;
+   [SerializeField] Animation RunStop;
+   
     void OnTriggerEnter(Collider other) 
     {
-      //Debug.Log($"{this.name} **Trigger by **{other.gameObject.name}");  
+      Debug.Log($"{this.name} **Trigger by **{other.gameObject.name}");  
       FreezeAction();
-           
+               
     }
+
+     
+
     void FreezeAction()
     {
+        lossee.Play();
+        RunStop.Stop();
+        GetComponent<MeshRenderer>().enabled= false;
         GetComponent<PlayerControl>().enabled = false;
         Invoke("ReloadLevel", freezetime);
     }
