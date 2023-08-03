23b7b632988781176581659743b9f503f804fd7c
leemk2310 <139373389+leemk2310@users.noreply.github.com>
2023-08-03 18:58:39 +0900

    Update paritcleAndEnemy.cs

--- Assets/Scrip/paritcleAndEnemy.cs
+++ Assets/Scrip/paritcleAndEnemy.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class paritcleAndEnemy : MonoBehaviour
 {
+    [SerializeField] GameObject gameVFX;
+
+    [SerializeField] Transform parent;
 
     void OnParticleCollision(GameObject other) 
     {
         Debug.Log($"{this.name}hit on + {other.gameObject.name}");
-         Destroy(gameObject);
+
+        GameObject VFX = Instantiate(gameVFX, transform.position, Quaternion.identity); // khởi tạo 1 đối tượng VFX
+        VFX.transform.parent = parent;  // gán đôi tượng  làm con của  thằng parent 
+                 Destroy(gameObject);     
     }
 }
