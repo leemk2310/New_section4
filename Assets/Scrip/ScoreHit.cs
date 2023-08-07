57182e2c8dd59a276d96f9996d5c075c956603f3
leemk2310 <lee.minhkhanh@gmail.com>
2023-08-07 18:46:27 +0900

    hit

--- Assets/Scrip/ScoreHit.cs
+++ Assets/Scrip/ScoreHit.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreHit : MonoBehaviour
 {
-   
-    [SerializeField]int score = 15;
 
+    int score;
+    TMP_Text scoreShow;
+
+     void Start()
+    {
+        scoreShow = GetComponent<TMP_Text>();
+        scoreShow.text = ("Start");
+    }
     public void IncreaseScore(int amountScore)
     {
         score = score + amountScore; // viet theo kieu basic
                                      //    score += amountScore; //viet theo kieu khac
+
         Debug.Log($" score is now {score}");
+        scoreShow.text = score.ToString();
          
     }
 }
